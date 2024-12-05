f5691c2d2a94646e1ff27436f21fefbee85aad06
Chris Le Sueur <c.m.lesueur@gmail.com>
2024-12-05 19:02:09 +0000

    Day 5: Use set instead of list for rules

--- src/05.hs
+++ src/05.hs
@@ -3,12 +3,17 @@
 module Main where
 
 --import Data.Array
+import Data.Set (Set, member)
+import qualified Data.Set as Set
+
 import Data.List
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as C
 
 import Advent
 import Advent.Input
+
+type Rules = Set (Int, Int)
 
 main :: IO ()
 main = do
@@ -17,9 +22,9 @@ main = do
     print $ part1 rules updates
     print $ part2 rules updates
 
-parse :: [B.ByteString] -> ([(Int, Int)], [[Int]])
+parse :: [B.ByteString] -> (Rules, [[Int]])
 parse input = let (rules, updates) = break B.null $ input in
-    (map parseRule rules, map parseUpdate $ filter (not . B.null) updates)
+    (Set.fromList $ map parseRule rules, map parseUpdate $ filter (not . B.null) updates)
 
 parseRule :: B.ByteString -> (Int, Int)
 parseRule line = let nums = map (fst . unwrap . C.readInt) $ B.split 124 line :: [Int] in -- '|'
@@ -28,23 +33,21 @@ parseRule line = let nums = map (fst . unwrap . C.readInt) $ B.split 124 line ::
 parseUpdate :: B.ByteString -> [Int]
 parseUpdate line = map (fst . unwrap . C.readInt) $ B.split 44 line -- ','
 
-part1 :: [(Int, Int)] -> [[Int]] -> Int
+part1 :: Rules -> [[Int]] -> Int
 part1 rules updates = sum $ [middle update| update <- updates, checkUpdate rules update]
 
 middle :: [Int] -> Int
 middle update = update !! (length update `div` 2)
 
-checkUpdate :: [(Int, Int)] -> [Int] -> Bool
+checkUpdate :: Rules -> [Int] -> Bool
 checkUpdate _ [] = True
-checkUpdate rules (a:bs) = and [checkOrder rules a b | b <- bs] && checkUpdate rules bs
+checkUpdate _ [a] = True
+checkUpdate rules (a:bs) = not ((head bs, a) `member` rules) && checkUpdate rules bs
 
-checkOrder :: [(Int, Int)] -> Int -> Int -> Bool
-checkOrder rules a b = not $ (b, a) `elem` rules
-
-part2 :: [(Int, Int)] -> [[Int]] -> Int
+part2 :: Rules -> [[Int]] -> Int
 part2 rules updates = sum $ [middle $ sortUpdates rules update| update <- updates, not $ checkUpdate rules update]
 
-sortUpdates :: [(Int, Int)] -> [Int] -> [Int]
+sortUpdates :: Rules -> [Int] -> [Int]
 sortUpdates rules update = sortBy (\a -> \b -> if
-        | (a, b) `elem` rules -> LT
+        | (a, b) `member` rules -> LT
         | otherwise -> GT) update
