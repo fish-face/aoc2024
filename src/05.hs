08a36db0b1f737507ab09f8144193960ba77bdaf
Chris Le Sueur <c.m.lesueur@gmail.com>
2024-12-05 23:45:26 +0000

    Maybe improve Day 5 more

--- src/05.hs
+++ src/05.hs
@@ -18,12 +18,14 @@ type Rules = Set (Int, Int)
 main :: IO ()
 main = do
     input <- readInputLines
-    let (rules, updates) = parse input
-    print $ part1 rules updates
-    print $ part2 rules updates
+    let
+        (rules, updates) = parse input
+        (good, bad) = partition (checkUpdate rules) updates
+    print $ part1 good
+    print $ part2 rules bad
 
 parse :: [B.ByteString] -> (Rules, [[Int]])
-parse input = let (rules, updates) = break B.null $ input in
+parse input = let (rules, updates) = break B.null input in
     (Set.fromList $ map parseRule rules, map parseUpdate $ filter (not . B.null) updates)
 
 parseRule :: B.ByteString -> (Int, Int)
@@ -33,8 +35,8 @@ parseRule line = let nums = map (fst . unwrap . C.readInt) $ B.split 124 line ::
 parseUpdate :: B.ByteString -> [Int]
 parseUpdate line = map (fst . unwrap . C.readInt) $ B.split 44 line -- ','
 
-part1 :: Rules -> [[Int]] -> Int
-part1 rules updates = sum $ [middle update| update <- updates, checkUpdate rules update]
+part1 :: [[Int]] -> Int
+part1 updates = sum $ [middle update| update <- updates]
 
 middle :: [Int] -> Int
 middle update = update !! (length update `div` 2)
@@ -45,9 +47,7 @@ checkUpdate _ [a] = True
 checkUpdate rules (a:bs) = not ((head bs, a) `member` rules) && checkUpdate rules bs
 
 part2 :: Rules -> [[Int]] -> Int
-part2 rules updates = sum $ [middle $ sortUpdates rules update| update <- updates, not $ checkUpdate rules update]
+part2 rules updates = sum $ [middle $ sortUpdates rules update| update <- updates]
 
 sortUpdates :: Rules -> [Int] -> [Int]
-sortUpdates rules update = sortBy (\a -> \b -> if
-        | (a, b) `member` rules -> LT
-        | otherwise -> GT) update
+sortUpdates rules = sortBy (\ a b -> (if (a, b) `member` rules then LT else GT))
