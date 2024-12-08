cc594c437bf58514896eead400666204e047e4e0
Chris Le Sueur <c.m.lesueur@gmail.com>
2024-12-08 20:08:12 +0000

    Fix day 5: don't delete blank lines in the middle of input

--- common/Advent/Input.hs
+++ common/Advent/Input.hs
@@ -2,6 +2,7 @@ module Advent.Input where
 
 import qualified System.Environment as SE
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
 
 readInput :: IO B.ByteString
 readInput = do
@@ -11,5 +12,10 @@ readInput = do
 readInputLines :: IO [B.ByteString]
 readInputLines = do
     args <- SE.getArgs
-    lines <- B.split 10 <$> B.readFile (head args)
-    return $ filter (not . B.null) lines
+    input <- B.readFile (head args)
+    let
+        stripped = C.strip input
+        lines = B.split 10 stripped
+--    lines <- B.split 10 <$> stripped
+    return lines
+--    return $ filter (not . B.null) lines
