a6ba1432b4b99640f6ef9bf8cee0183356454e13
Chris Le Sueur <c.m.lesueur@gmail.com>
2024-12-12 00:06:13 +0000

    Hopefully fix d11 in docker

--- src/11.hs
+++ src/11.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import Data.Function.Memoize
+import Data.MemoUgly
 import Data.Maybe
 import qualified Data.ByteString.Char8 as C
 
@@ -17,24 +17,21 @@ main = do
     let
         nums = map readInt $ C.words $ C.strip input
     print $ sum $ map (numStones 25) nums
-    print $ sum $ map (memoize numStones 75) nums
+    print $ sum $ map (numStones 75) nums
 
 numStones :: Int -> Int -> Int
-numStones = memoize2 numStones'
+numStones = curry $ memo  $ uncurry numStones'
     where
-        numStones' 0 stone = 1
+        numStones' 0 _ = 1
         numStones' n stone =
             let
-                digs = digits stone
-                ndigs = C.length digs
-                (as, bs) = C.splitAt (ndigs `div` 2) digs
-                a = readInt as
-                b = readInt bs
+                d = ndigits stone
+                (a, b) = splitdigits d stone
             in
             if
-            | stone == 0 -> numStones (n-1) 1 --`debug` ((replicate n ' ') ++ "zero " ++ (show 1))
-            | ndigs `mod` 2 == 0 -> numStones (n-1) a + numStones (n-1) b --`debug` ((replicate n ' ') ++ "split " ++ (show (a, b)))
-            | otherwise -> numStones (n-1) (stone * 2024) --`debug` ((replicate n ' ') ++ "mul " ++ (show (stone * 2048)))
+            | stone == 0 -> numStones (n-1) 1
+            | d `mod` 2 == 0 -> numStones (n-1) a + numStones (n-1) b
+            | otherwise -> numStones (n-1) (stone * 2024)
 
-digits :: Int -> C.ByteString
-digits num = C.pack $ show num
+ndigits num = ceiling (logBase 10 (fromIntegral (num + 1)))
+splitdigits digits num = divMod num (10 ^ (digits `div` 2))
