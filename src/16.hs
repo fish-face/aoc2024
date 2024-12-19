979b7fc393babb269dd18294c0503443a8bd1c64
Chris Le Sueur <c.m.lesueur@gmail.com>
2024-12-19 10:55:46 +0000

    Day 16 p2

--- src/16.hs
+++ src/16.hs
@@ -2,8 +2,6 @@
 
 module Main where
 
-import Debug.Trace
-
 import Data.List
 import Data.Maybe
 import Data.Array ((!), (//), assocs, bounds)
@@ -33,10 +31,9 @@ main = do
         (_, (width, height)) = bounds grid
         start = findStart input
         end = (width - 1, 1)
-        path = pathFind grid start end
-    print start
-    print end
-    print path
+        (part1, part2) = pathFind grid start end
+    print part1
+    print part2
 
 findStart :: [C.ByteString] -> Coord
 findStart gridLines = let
@@ -51,42 +48,51 @@ findIV p = find (\(i, v) -> p v) . zip [0..]
 
 type Position = (Direction, Coord)
 
---type State = (Int, Vector Coord, Coord)
 data State = State {
     seen :: Set Position,
-    distance :: Map Position Int,
+    distance :: Map Position (Int, [[Coord]]),
     queue :: Q.MinQueue (Int, Position)
 }
 
-pathFind :: Grid Char -> Coord -> Coord -> Int
+pathFind :: Grid Char -> Coord -> Coord -> (Int, Int)
 pathFind grid start end = let
         seen = S.empty
-        distance = M.singleton (East, start) 0
+        distance = M.singleton (East, start) (0, [[start]])
         queue = Q.singleton (0, (East, start))
-        result = traceShowId $ go (State seen distance queue)
-    in fromJust $ lookupAnyDir result end
+        result = dijkstra (State seen distance queue)
+        result' = fromJust $ lookupAnyDir result end
+        -- for part2
+        allPoints = S.unions $ map (S.fromList) (snd result') :: Set Coord
+    in (fst result', S.size allPoints)
     where
-        go :: State -> Map Position Int
-        go state@(State seen distance queue) = if Q.null queue then distance `debug` "ran out"
+        dijkstra :: State -> Map Position (Int, [[Coord]])
+        dijkstra state@(State seen distance queue) = if Q.null queue then distance
             else let
                     ((d, pos@(_, p)), queue1) = Q.deleteFindMin queue
                 in
                 if
-                    | p == end -> distance `debug` "success"
-                    | S.member pos seen -> go (state {queue=queue1}) `debug` "already seen"
+                    | p == end -> distance
+                    | S.member pos seen -> dijkstra (state {queue=queue1})
                     | otherwise -> let
                             seen1 = S.insert pos seen
                             nexts = getNeighbourCosts pos
                             unvisited = filter (\(d1, p) -> not $ S.member p seen1) nexts
                         in
-                        go $ foldl (foldNeighbour pos) (State seen1 distance queue1) unvisited
+                        dijkstra $ foldl (foldNeighbour pos) (State seen1 distance queue1) unvisited
         foldNeighbour :: Position -> State -> (Int, Position) -> State
-        foldNeighbour pos state@(State seen1 distance queue1) (stepCost, neighbour) = let
-                newDist = distance M.! pos + stepCost
-                oldDist = distance M.! neighbour --`debug` (show newDist)
-            in if not (M.member neighbour distance) || newDist < oldDist
-                then State seen1 (M.insert neighbour newDist distance) (Q.insert (newDist, neighbour) queue1)
-                else state
+        foldNeighbour pos@(d, p) state@(State seen1 distance queue1) (stepCost, neighbour@(nd, np)) = let
+                (cost1, paths1) = distance M.! pos
+                newCost = cost1 + stepCost
+                -- prepend new point to all shortest paths to current point
+                newPaths = map (np:) paths1
+                (oldCost, oldPaths) = distance M.! neighbour
+            in if
+                | not (M.member neighbour distance) || newCost < oldCost ->
+                    State seen1 (M.insert neighbour (newCost, newPaths) distance) (Q.insert (newCost, neighbour) queue1)
+                | newCost == oldCost ->
+                    -- don't need to enqueue neighbour if we just found an alternative path; we will search anyway
+                    State seen1 (M.insert neighbour (newCost, newPaths ++ oldPaths) distance) queue1
+                | otherwise -> state
         getNeighbourCosts :: Position -> [(Int, Position)]
         getNeighbourCosts (d, p) = let
                 dirs = [rotateDirR d, rotateDirL d]
@@ -94,5 +100,5 @@ pathFind grid start end = let
                 coords = if grid ! p1 /= '#' then [(1, (d, p1))] else []
             in
             coords ++ [(1000, (rotateDirR d, p)), (1000, (rotateDirL d, p))]
-        lookupAnyDir :: Map Position Int -> Coord -> Maybe Int
+        lookupAnyDir :: Map Position a -> Coord -> Maybe a
         lookupAnyDir m pos = firstJusts $ map (\d -> M.lookup (d, pos) m) [North, East, South, West]
